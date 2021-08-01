cbe433e4d353960c8036a1eb7717d1527e78a1c7
Kamil <40839443+kamil34@users.noreply.github.com>
2021-08-01 22:02:49 +0200

    Error codes

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// responses
+const responses = {
+    error: require('./responses/error')
+}
+
 app.get('/', (request, response) => {
     response.send("The '/' of the server.")
     })
