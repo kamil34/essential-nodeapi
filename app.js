7903902505719ae8b21ffcd50c30e4e830f85fb4
Kamil <40839443+kamil34@users.noreply.github.com>
2021-08-05 19:34:35 +0200

    Concept improvement
    IDEA #1 (Aug 5, 2021):
    1. A `for` loop with checking if there is such data in `error_messages` array,
    2. Providing index by assigning `for loop` result to a variable in what index to search `message` from,
    2.1. Throwing the response with requesting data such as `status code, status message`
    3. `else` => throw the `static` response.

--- app.js
+++ app.js
@@ -31,11 +31,13 @@ app.get('/', (request, response) => {
 // errors
 const error = new Error();
 
-// error handling
-app.use((request, response, next) => {
+// 404 error handling
+app.use((request, response) => {
     error.statusCode = 404;
     throw error;
     });
+
+// errors
 app.use((error, request, response, next) => {
 
     // const error_messages = {
@@ -48,27 +50,54 @@ app.use((error, request, response, next) => {
         {
             code: 400,
             message: "Bad request."
-        },
-        {
-            code: 404,
-            message: "Not found."
-        },
-        {
-            code: 404,
-            message: "Internal Server Error."
         }
+        // {
+        //     code: 404,
+        //     message: "Not found."
+        // },
+        // {
+        //     code: 500,
+        //     message: "Internal Server Error."
+        // }
     ];
+
     // Issue: Different expectations towards response.
     // Code:
     // Object.keys(error_messages).find(error_messages => error_messages.code === error.statusCode).message;
+    
+    // IDEA #1 (Aug 5, 2021):
+    // 1. A `for` loop with checking if there is such data in `error_messages` array,
+    // 2. Providing index by assigning `for loop` result to a variable in what index to search `message` from,
+    // 2.1. Throwing the response with requesting data such as `status code, status message`
+    // 3. `else` => throw the `static` response.
 
-    response.status(error.statusCode || 500);
-    // response.json({
-    //       'success': false,
-    //       'code': error.statusCode,
-    //       'info': error.statusCode, //todo
-    //       'timestamp': moment().format("hh:mm:ss.SSS a, MM/DD/YYYY")
-    //       });
+    for(var i=0; i<Object.keys(error_messages).length; i++) {
+        if(error_messages[i].code == error.statusCode) {
+
+            error.statusMessage = error_messages[i].message;
+            response.status(error.statusCode);
+            response.json({
+                'success': false,
+                'code': error.statusCode,
+                'info': error.statusMessage,
+                'timestamp': moment().format("hh:mm:ss.SSS a, MM/DD/YYYY")
+                });
+
+            break;
+            }
+        else {
+
+            response.status(404 || 500);
+            response.json({
+                'success': false,
+                'code': response.statusCode,
+                'info': 'Not found / Internal Server Error.',
+                'timestamp': moment().format("hh:mm:ss.SSS a, MM/DD/YYYY")
+                });
+                
+            }
+        }
     });
+        
 
 app.listen(8000);
